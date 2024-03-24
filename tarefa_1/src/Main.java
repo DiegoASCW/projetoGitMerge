9c9e453e415daae0f036d2aa66d82e1ee3fbed67
DiegoASCW <diegolautens@outlook.com>
2024-03-24 15:47:19 -0300

    Tarefa 1 [FINALIZADA]

---
+++ tarefa_1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+        menu.imprimirOpcoes();
+
+        Usuarios diego = new Usuarios("Diego Lautenschlager", 20, "***", 194, "55041");
+        Usuarios sandro = new Usuarios("Sandro", 23, "***", 190, "55041");
+        Usuarios pedro = new Usuarios("Pedro", 25, "***", 180, "55041");
+
+    }
+}
---
+++ tarefa_1/src/Usuarios.java
@@ -0,0 +1,16 @@
+public class Usuarios {
+
+    private String nome;
+    private int idade;
+    private String cpf;
+    private int alturaCM;
+    private String telefone;
+
+    public Usuarios(String nome, int idade, String cpf, int alturaCM, String telefone){
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.alturaCM = alturaCM;
+        this.telefone = telefone;
+    }
+}
